ced39a212ec98c0a85e6078716a3190fa0fcd60e
AntonioSouls <tonyplanza1@gmail.com>
2025-05-31 22:03:49 +0200

    Migliorato il database_cleaner con una logica di pulizia migliore

--- scripts/database_cleaner.py
+++ scripts/database_cleaner.py
@@ -5,20 +5,16 @@ print("Starting Dataset Cleaning ...")
 chunks = pd.read_csv("data/used_cars_data.csv", chunksize=100000, low_memory=False)
 df = pd.concat(chunks, ignore_index=True)
 
-
 # Seleziono le colonne da mantenere e faccio pulizia
 print("Selecting the columns to be maintained...")
 df = df[["engine_displacement", "horsepower", "make_name", "model_name", "power", "price", "year"]]
-print("Deleting NaN Values ...")
-df.dropna(subset=["make_name", "model_name", "price", "year", "horsepower", "engine_displacement"], inplace=True) # Elimino le righe che hanno NaN in almeno una di queste colonne
+
 print("Removing duplicates ...")
 df.drop_duplicates(inplace=True) # Rimuovo i duplicati, ossia righe con stessi valori su tutte le colonne
 
-# Rimuovo eventuali valori improbabili
-print("Removal of improbable values")
-df = df[df["price"] > 500]
-df = df[(df["year"] >= 1980)]
-df = df[(df["horsepower"] > 20) & (df["horsepower"] < 1500)]
+df['year'] = pd.to_numeric(df['year'], errors='coerce')
+df = df[df['year'] >= 1000]
+df['year'] = df['year'].astype(int)
 
 # Uniformo i nomi
 print("Unification of names ...")
