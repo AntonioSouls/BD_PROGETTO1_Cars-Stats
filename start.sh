8e3d0e41d7fbbaa738b95ea0310a0949fda1f6e4
AntonioSouls <tonyplanza1@gmail.com>
2025-06-02 12:19:08 +0200

    Migliorato lo script start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 # Command to run for starting the virtual environment and HDFS automatically
 
 source venv/bin/activate
+$HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
+cd data
+hdfs dfs -mkdir -p /user/$USER/data
+hdfs dfs -put vehicles_clean.csv /user/$USER/data
